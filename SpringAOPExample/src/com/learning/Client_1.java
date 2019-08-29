8f862ee41bb7f6baa20f29b5965cc3f7666643cf
Santosh Kumar Kar <santoshkumarkar@Santoshs-MacBook-Pro.local>
2019-08-29 18:44:12 +0530

    Updaged the AOP programs

--- SpringAOPExample/src/com/learning/Client_1.java
+++ SpringAOPExample/src/com/learning/Client_1.java
@@ -3,11 +3,16 @@ package com.learning;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Client {
+public class Client_1 {
 	
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-		StudentServiceImpl service = (StudentServiceImpl) ctx.getBean("studentService");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("ApplicationContext_1.xml");
+		
+		/*
+		 * Notice here, we are using the proxy bean, but NOT the service bean 
+		 * Because we have used <b>ProxyFactoryBean</b>
+		 */
+		StudentServiceImpl service = (StudentServiceImpl) ctx.getBean("studentServiceProxy");
 		service.showStudentList();
 	}
 }
---
+++ SpringAOPExample/src/com/learning/Client_2.java
@@ -0,0 +1,18 @@
+package com.learning;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client_2 {
+	
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("ApplicationContext_2.xml");
+		
+		/*
+		 * Notice here, we are using the service bean, but NOT the proxy bean 
+		 * Because we have used BeanNameAutoProxyCreator which is taking care of proxy and bean
+		 */
+		StudentServiceImpl service = (StudentServiceImpl) ctx.getBean("studentService");
+		service.showStudentList();
+	}
+}
